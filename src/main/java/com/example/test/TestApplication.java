7d04be05e0b6c1c02a2b4318ed7d3677b38cf4e5
lvdm <lvdemingzz@163.com>
2020-06-06 14:03:44 +0800

    WIP:将本地项目添加到GIT库

---
+++ src/main/java/com/example/test/TestApplication.java
@@ -0,0 +1,27 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("Hello");
+        return "Yes, You got Me!";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "This is test2";
+    }
+
+}
